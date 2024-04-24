84c3bbb58c3abbf22696d17ad652b6f3633c5f63
An7o7 <140371014+An7o7@users.noreply.github.com>
2024-04-24 18:56:47 +0900

    Title: 살 수 있는 물건 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240424/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+
+    if (a>= 3000){
+        cout << "book";
+    }
+    else if (a>=1000){
+        cout << "mask";
+    }
+    else if (a>= 500){
+        cout << "pen";
+    }
+    else{
+        cout << "no";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
