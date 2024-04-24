a8d78d976cee4e175cf02b12bcbbc929fc7b8d28
An7o7 <140371014+An7o7@users.noreply.github.com>
2024-04-24 18:03:35 +0900

    Title: 2개 중 최대, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240424/2개 중 최대/max-of-two-nums.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cin >> a >> b;
+
+    
+    if (a<b){
+        cout << b;
+    }
+    else if (a>b){
+        cout << a;
+    }
+    else cout << a;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
