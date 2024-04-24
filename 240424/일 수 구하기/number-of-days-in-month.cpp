c7ef4d9bec09e77abd5690dff30f61bb29a08874
An7o7 <140371014+An7o7@users.noreply.github.com>
2024-04-24 21:45:11 +0900

    Title: 일 수 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240424/일 수 구하기/number-of-days-in-month.cpp
+++ 240424/일 수 구하기/number-of-days-in-month.cpp
@@ -3,21 +3,17 @@ using namespace std;
 
 int main() {
     int month;
-    
+    int days;
+
     // 월 입력 받기
     cout << "월을 입력하세요: ";
     cin >> month;
-    
-    // 해당 월의 일수 계산 및 출력
-    if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
-        cout << "해당 월은 31일까지 있습니다.";
-    } else if(month == 4 || month == 6 || month == 9 || month == 11) {
-        cout << "해당 월은 30일까지 있습니다.";
-    } else if(month == 2) {
-        cout << "해당 월은 28일까지 있습니다.";
-    } else {
-        cout << "올바른 월을 입력하세요.";
+
+    // 해당 월의 일수 계산
+    if (month == 2) {
+        days = 28;
+    } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+        days = 30;
+    } else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+        days = 31;
     }
-    
-    return 0;
-}
