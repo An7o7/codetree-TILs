ded99d245d8937fbb8ffb6d32f5abb424c375fe5
An7o7 <140371014+An7o7@users.noreply.github.com>
2024-04-24 19:57:37 +0900

    Title: 세 정수의 최솟값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240424/세 정수의 최솟값/minimum-of-three-numbers.cpp
+++ 240424/세 정수의 최솟값/minimum-of-three-numbers.cpp
@@ -4,7 +4,7 @@ int main() {
     int a,b,c;
     cin >> a >> b >> c;
 
-    cout << ((a>=b)? b>=c? c:b : a>=c ? c : b);
+    cout << ((a>=b)? b>=c? c:b : a>=c ? c : a);
     // 여기에 코드를 작성해주세요.
     return 0;
 }
