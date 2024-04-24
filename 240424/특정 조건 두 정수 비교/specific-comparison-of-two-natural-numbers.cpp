8e87803f363c44895a1c00438e05aa0c4afc79ff
An7o7 <140371014+An7o7@users.noreply.github.com>
2024-04-24 19:11:14 +0900

    Title: 특정 조건 두 정수 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240424/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
+++ 240424/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
@@ -7,11 +7,13 @@ int main() {
     if (a<b){
         cout << 1 << " ";
     }
+    else
+    cout << 0 << " ";
+    
     if (a==b){
         cout << 1 << " ";
     }
-    else
-    cout << 0;
+    
     
     // 여기에 코드를 작성해주세요.
     return 0;
