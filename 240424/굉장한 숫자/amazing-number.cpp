f8188f638d6e4046faea9469e4d661917b4d1472
An7o7 <140371014+An7o7@users.noreply.github.com>
2024-04-24 20:20:57 +0900

    Title: 굉장한 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240424/굉장한 숫자/amazing-number.cpp
+++ 240424/굉장한 숫자/amazing-number.cpp
@@ -7,7 +7,11 @@ int main() {
     if (a%2 != 0 && a%3 == 0){
         cout << "true";
     }
+    else if (a%2==0 && a%5==0){
+        cout << "true";
+    }
     else cout << "false";
+    
     // 여기에 코드를 작성해주세요.
     return 0;
 }
