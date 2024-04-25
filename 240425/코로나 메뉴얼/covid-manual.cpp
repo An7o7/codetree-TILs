d6945fe5a72fffcd92f0df579392c207ff67dcc2
An7o7 <140371014+An7o7@users.noreply.github.com>
2024-04-25 10:18:08 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240425/코로나 메뉴얼/covid-manual.cpp
+++ 240425/코로나 메뉴얼/covid-manual.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
+
 using namespace std;
+
 int main() {
-    char a,b,c;
-    int a1,b1,c1, e=0;
-
-    cin >> a >> a1 >> b >> b1  >> c >> c1;
-
-    if (a=='Y' && a >= 37){
-        e+=1;
-    }
-    if (b=='Y' && b >= 37){
-        e+=1;
-    }
-    if (c=='Y' && c >= 37){
-        e+=1;
-    }
-    if ( e >= 2){
-        cout << 'E';
+    // 변수 선언
+    char c1, c2, c3;
+    int t1, t2, t3;
+    
+    // 입력
+    cin >> c1 >> t1;
+    cin >> c2 >> t2;
+    cin >> c3 >> t3;
+    
+    // A가 2명 이상인지 판단하기
+    if(c1 == 'Y' && t1 >= 37) {
+        // 첫 번째 사람이 A라면, 남은 두 사람 중 한 사람이라도 A면 됩니다.
+        if((c2 == 'Y' && t2 >= 37) || (c3 == 'Y' && t3 >= 37))
+            cout << "E";
+        else
+            cout << "N";
     }
     else {
-        cout << 'N';
+        // 첫 번째 사람이 A가 아니라면, 남은 두 사람 모두 A여야만 합니다.
+        if((c2 == 'Y' && t2 >= 37) && (c3 == 'Y' && t3 >= 37))
+            cout << "E";
+        else
+            cout << "N";
     }
-    // 여기에 코드를 작성해주세요.
     return 0;
 }
