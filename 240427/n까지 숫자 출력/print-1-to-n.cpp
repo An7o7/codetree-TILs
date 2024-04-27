8a45bc9eb1c34f7ad91ecf0f27e166070316edc0
An7o7 <140371014+An7o7@users.noreply.github.com>
2024-04-27 21:11:40 +0900

    Title: n까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240427/n까지 숫자 출력/print-1-to-n.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int i = 1;
+    while (i <=10){
+        cout << i << ' ';
+        i++;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
