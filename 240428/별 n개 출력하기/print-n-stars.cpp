8300786fda8f71384711c964cd73e5d195b2d594
An7o7 <140371014+An7o7@users.noreply.github.com>
2024-04-28 08:02:48 +0900

    Title: 별 n개 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240428/별 n개 출력하기/print-n-stars.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int a=0;
+    while (a<n){
+        cout << '*' << endl;
+        a++;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
